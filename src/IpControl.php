7e3cc912da96b491bdf8f2c8774fe8da6e5b8d20
Luis Cajigas Martínez <luic87@gmail.com>
2016-10-25 13:00:05 +0200

    Middleware finished

--- src/IpControl.php
+++ src/IpControl.php
@@ -1,36 +1,65 @@
 <?php
 namespace Luisinder\Middleware;
+
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+
 Class IpControl
 {
   protected $trustedIp;
-	public function __construct(array $trustedIp = [])
-	{
-    $this->trustedIp = $trustedIp;
-	}
-	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
-	{
-		if (!$next) {
-			return $response;
-		}
+  protected $redirectUrl;
 
-    if(!($this->checkIp($request->$req->getAttribute('ip_address'))))
+  public function __construct(array $trustedIp = [], string $redirectUrl = "/")
+  {
+    $this->trustedIp = $trustedIp;
+    $this->redirectUrl = $redirectUrl;
+  }
+
+  public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
+  {
+    if (!$next)
     {
-      $response = $response
-                ->withStatus(401)
-                ->withHeader("WWW-Authenticate", sprintf('Basic realm="%s"', $this->options["realm"]));
-    }
-    else
-    {
-      return $response = $next($request, $response);
-    }
-	}
-	protected function checkIp($ip)
-	{
-		if(array_search($ip,$this->trustedIp)== FALSE)
-      return false;
-    else
-      return true;
-	}
+     return $response;
+   }
+
+   $ipAddress = $this->determineClientIpAddress($request);
+
+   if(!($this->checkIp($ipAddress)))
+   {
+    return $response->withRedirect($this->redirectUrl);
+  }
+  else
+  {
+    return $response = $next($request, $response);
+  }
+}
+
+protected function checkIp($ip)
+{
+  if(array_search($ip,$this->trustedIp) == FALSE)
+    return false;
+  else
+    return true;
+}
+
+protected function determineClientIpAddress($request)
+{
+  $ipAddress = null;
+  $serverParams = $request->getServerParams();
+  if (isset($serverParams['REMOTE_ADDR']) && $this->isValidIpAddress($serverParams['REMOTE_ADDR']))
+  {
+    $ipAddress = $serverParams['REMOTE_ADDR'];
+  }
+  return $ipAddress;
+}
+
+protected function isValidIpAddress($ip)
+{
+  $flags = FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6;
+  if (filter_var($ip, FILTER_VALIDATE_IP, $flags) === false)
+  {
+    return false;
+  }
+  return true;
+}
 }
